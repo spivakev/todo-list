ce5f3e264fdde27025dc6b093b49e5ee4905b019
Elena Spivak <spivak.e.v@gmail.com>
2020-10-12 22:31:55 +0300

    feat: add Task component

---
+++ src/components/Task/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: props.taskText
+    }
+
+  }
+
+  render() {
+    return (
+      <li>{}</li>
+    )
+  }
+}
