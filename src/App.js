64b6208cd9838b4b700a7ae7043153319e56de2f
Elena Spivak <spivak.e.v@gmail.com>
2020-10-12 22:34:29 +0300

    feat: add App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from './components/Form'
+import Form from './components/Task'
+import Form from './components/TaskList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  addTaskToList = (e) => {
+    e.preventDefault();
+    let newTask = e.target.elements.task.value;
+    console.log(newTask);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <TaskList />
+        <Form addTask={this.addTaskToList} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
+
