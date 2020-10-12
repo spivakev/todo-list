33bbaf3a64e71e56a635846e8c93f22473dde13a
Elena Spivak <spivak.e.v@gmail.com>
2020-10-12 23:13:45 +0300

    feat: add data file (for testing)

---
+++ src/prevtasks.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    "id": "01",
+    "text": "Помыть посуду",
+    "done": "false"
+  },
+  {
+    "id": "02",
+    "text": "Погулять с котом",
+    "done": "false"
+  },
+  {
+    "id": "03",
+    "text": "Написать React приложение",
+    "done": "true"
+  }
+]
