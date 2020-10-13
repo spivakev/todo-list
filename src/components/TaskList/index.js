22bc850e1875e134fb48878d573d03ac358e148d
Elena Spivak <spivak.e.v@gmail.com>
2020-10-14 00:03:26 +0300

    feat: add task-list styles

--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Task from '../Task';
+import './style.css';
 
 class TaskList extends React.Component {
 
@@ -11,7 +12,9 @@ class TaskList extends React.Component {
       </li>
     )
 
-    return (allTasks)
+    return (
+      <ul className="task-list">{allTasks}</ul>
+    )
   }
 }
 
