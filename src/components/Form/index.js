e1c9ace78d8afe3fe95714d237068519636ef53d
Elena Spivak <spivak.e.v@gmail.com>
2020-10-13 23:27:09 +0300

    feat: add classnames for the form elements

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Form = props => (
   <div>
-    <form onSubmit={props.addTask}>
-      <button>+</button>
-      <input type="text" name="task" placeholder="Текст новой задачи" />
+    <form className="form" onSubmit={props.addTask}>
+      <button className="form__btn">+</button>
+      <input className="form__input" type="text" name="task" placeholder="Текст новой задачи" />
     </form>
   </div>
 )
