6171353e3d9d11ad71126e3a1a19a7ac726fb0ae
Elena Spivak <spivak.e.v@gmail.com>
2020-10-13 23:23:00 +0300

    feat: add styles for task list items

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css';
 
 const Task = props => (
   <span>{props.text}</span>
--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -5,7 +5,7 @@ class TaskList extends React.Component {
 
   render() {
     const allTasks = this.props.tasks.map(task =>
-      <li key={task.id}>
+      <li className="task-list__li" key={task.id}>
         <Task text={task.text}
         />
       </li>
