4cb035b29681d736f2b969b603df98c45983f206
Elena Spivak <spivak.e.v@gmail.com>
2020-10-14 00:07:43 +0300

    feat: add svg icon for the input form button

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PlusIcon from "../../icons/PlusIcon"
+import './style.css';
 
 const Form = props => (
   <div>
     <form className="form" onSubmit={props.addTask}>
-      <button className="form__btn">+</button>
+      <button className="form__btn"><PlusIcon/></button>
       <input className="form__input" type="text" name="task" placeholder="Текст новой задачи" />
     </form>
   </div>
