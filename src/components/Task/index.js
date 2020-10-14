688c5d0b630d8643b0fe824b26b7cd5c20067e1c
Elena Spivak <spivak.e.v@gmail.com>
2020-10-14 21:34:12 +0300

    feat: add styles for checkbox toggle

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './style.css';
 
 const Task = props => (
-  <span>{props.text}</span>
+  <label className="task">
+    <input className="task__checkbox" type="checkbox" />
+    <span className="task__icon"></span>
+    <span className="task__text">{props.text}</span>
+  </label>
 )
 
 export default Task;
